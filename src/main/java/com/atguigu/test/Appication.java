910855a88709f63cad5a847ed5519ec5dddbb218
aihuan <dnsip128@163.com>
2022-03-28 20:55:22 +0800

    forth commit newBranch

--- src/main/java/com/atguigu/test/Appication.java
+++ src/main/java/com/atguigu/test/Appication.java
@@ -8,5 +8,6 @@ public class Appication {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("hello");
+        System.out.println("newBranch");
     }
 }
