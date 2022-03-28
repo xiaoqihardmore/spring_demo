7da665596fcb5b0a62aebf90f616dca1312462fb
aihuan <dnsip128@163.com>
2022-03-28 20:45:41 +0800

    second commit

---
+++ src/main/java/com/atguigu/test/Appication.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @ author: Nie
+ * @ version: 1.0
+ */
+public class Appication {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
